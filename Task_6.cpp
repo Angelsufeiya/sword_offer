81d945c0103d8dc54dee6a323692843a53f9bfe8
Angelsufeiya <Angelsufeiya@hotmail.com>
2020-10-26 22:39:24 +0800

    10-26

--- Task_6.cpp
+++ Task_6.cpp
@@ -2,12 +2,3 @@
 #include <stack>
 using namespace std;
 
-struct ListNode{
-    int m_value;
-    ListNode* m_next;
-}ListNode;
-
-void PrintListReversing(ListNode* Head){
-    // 从尾到头打印链表
-    
-}
---
+++ Test.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct ListNode{
+    int m_value;
+    struct ListNode * m_next;
+}ListNode;
+
+//void PrintListReversing(ListNode* Head){
+    // 从尾到头打印链表
+//}
+
+void Insert(ListNode * head, int val){
+    ListNode * temp = (ListNode *)malloc(sizeof(ListNode));
+    temp->m_value = val;
+#if 1
+    // 头插
+    temp->m_next = head->m_next;
+    head->m_next = temp;
+#else
+    // 尾插
+    ListNode * p = head;
+    while(p->m_next != NULL){
+        p = p->m_next;
+    }
+    p->m_next = temp;
+    temp->m_next = NULL;
+#endif
+}
+
+void Print(ListNode * head){
+    ListNode * temp = head->m_next;
+    while(temp){
+        printf("%d->", temp->m_value);
+        temp = temp->m_next;
+    }
+    printf("NULL\n");
+}
+
+void Free(ListNode * head){
+    ListNode * p = head;
+    ListNode * temp;
+    while(p){
+        temp = p->m_next;
+        free(temp);
+        p = temp;
+    }
+}
+
+ListNode *init(){
+    ListNode * temp = (ListNode *)malloc(sizeof(ListNode));
+    temp->m_next = NULL;
+    return temp;
+}
+
+int main(){
+    ListNode * Head = init();
+    for(int i = 1; i < 10; ++i){
+        Insert(Head, i);
+    }
+    Print(Head);
+    Free(Head);
+    return 0;
+}
