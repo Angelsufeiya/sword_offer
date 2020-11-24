18a85eb4988d8fc243966494728363c4e3565b54
Angelsufeiya <Angelsufeiya@hotmail.com>
2020-11-24 13:41:15 +0800

    11-24

--- Test.cpp
+++ Test.cpp
@@ -2,6 +2,58 @@
 #include <vector>
 
 using namespace std;
+#if 0
+//小顶堆结点调整
+void adjustMinHeap(vector<int>& nums,int root,int len) {
+    int l_ch=2*root+1;  //左子结点
+    int r_ch=l_ch+1;   //右子结点
+    int index=root;  //较大结点
+ 
+    if(r_ch < len && nums[r_ch] < nums[index]) index=r_ch; 
+    if(l_ch < len && nums[l_ch] < nums[index]) index=l_ch;
+ 
+    if(index != root) //当前结点非最小结点
+    {
+        swap(nums[index],nums[root]);
+        adjustMinHeap(nums,index,len);
+    }
+    return;
+}
+ 
+vector<int> TopKInHeap(vector<int>& nums,int k,int len){
+    vector<int>res(nums.begin(),nums.begin()+k); //取出前k个数
+ 
+    for(int i=k/2-1;i>=0;i--)  //根据前K个数建立一个小顶堆
+    {
+        adjustMinHeap(res,i,k);
+    }
+ 
+    //将剩下的数与堆顶做比较
+    for(int i=k;i<len;i++){
+        if(nums[i]>res[0])  //当前数比堆顶数大
+        {
+            res[0]=nums[i]; //将堆顶更新为该数
+            adjustMinHeap(res,0,k); //重新调整堆
+        }
+    }
+    return res;
+}
+
+int main(){
+    int arr[] = { 6, 1, 2, 7, 9, 3, 4, 5, 10, 8 };
+    vector<int> v(arr, arr+10);
+
+    int k = 5;
+    cout << endl <<  "查找前K个大小的数字：";
+    vector<int> res = TopKInHeap(v, k, 10);
+    for(int i = 0; i < k; ++i) {
+        if(i == 0) cout << res[i];
+        else cout << ' ' << res[i];
+    }
+    return 0;
+}
+#endif
+#if 1
 
 void quickSort(vector<int>& v, int l, int r){
     if(l > r) return;
@@ -28,25 +80,23 @@ int QuickSort(vector<int>& v, int left, int right, int k){
     v[left] = v[i];
     v[i] = tmp;
 
-    if(i == k-1) return v[i];
+    if(i == k-1) return i;
     else if(i < k-1) return QuickSort(v, i+1, right, k);
     else return QuickSort(v, left, i-1, k);
 }
 
 int main(){
-    int arr[] = { 6, 1, 2, 7, 9, 3, 4, 5, 10, 8 };
+    int arr[] = { 6, -1, 12, 71, 29, 33, 41, 25, 110, 8 };
     vector<int> v(arr, arr+10);
     //quickSort(v, 0, 9);
-    for(int i = 0; i < v.size(); ++i){
-        cout << v[i] << ' ';
-    }
+    //for(int i = 0; i < v.size(); ++i)cout << v[i] << ' ';
 
-    int k = 8;
-    cout << endl <<  "查找第K个大小的数字：";
-    cout << QuickSort(v, 0, 9, k) << endl;
+    int k = 3;
+    cout <<  "查找前K个大小的数字：";
+    for(int i = QuickSort(v, 0, 9, 10-k+1); i < v.size(); ++i) cout << v[i] << ' ';
     return 0;
 }
-
+#endif
 #if 0
 int main(){
     double d = 12.34;
