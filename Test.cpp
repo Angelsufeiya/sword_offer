583d82789b4b3827ef19c60a077316cd024fa876
Angelsufeiya <Angelsufeiya@hotmail.com>
2021-01-13 19:21:41 +0800

    01-13

--- Test.cpp
+++ Test.cpp
@@ -4,6 +4,34 @@
 using namespace std;
 
 
+
+#if 0
+
+/*
+struct TreeNode {
+	int val;
+	struct TreeNode *left;
+	struct TreeNode *right;
+	TreeNode(int x) :
+			val(x), left(NULL), right(NULL) {
+	}
+};*/
+class Solution {
+public:
+    bool dfs(TreeNode* r1, TreeNode* r2){
+        if(!r2) return true;
+        if(!r1) return false;
+        return r1->val==r2->val && dfs(r1->left,r2->left) && dfs(r1->right,r2->right);
+    }
+    bool HasSubtree(TreeNode* pRoot1, TreeNode* pRoot2){
+        if(!pRoot1 || !pRoot2) return false;
+        
+        return dfs(pRoot1,pRoot2) || HasSubtree(pRoot1->left,pRoot2) || HasSubtree(pRoot1->right,pRoot2);
+    }
+};
+
+#endif
+
 #if 0
 // 对称的二叉树
 /*
@@ -59,6 +87,7 @@ public:
 #endif
 
 #if 0
+// 镜像二叉树
 /*
 struct TreeNode {
 	int val;
