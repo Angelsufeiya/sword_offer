7e17c8bce03602b79eb253cd11b11b1c5f809856
Angelsufeiya <Angelsufeiya@hotmail.com>
2021-02-24 23:29:42 +0800

    02-24

--- Everyday.cpp
+++ Everyday.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 // 删除排序数组中的重复项
+/*
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
@@ -17,3 +18,41 @@ public:
         return res+1;
     }
 };
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        return distance(nums.begin(), unique(nums.begin(), nums.end()));
+    }
+};
+*/
+// 删除排序数组中的重复项II
+/*
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if(nums.size() <= 2) return nums.size();
+        int res = 2;
+        for(int i = 2; i < nums.size(); ++i){
+            if(nums[res-2] != nums[i]){
+                nums[res++] = nums[i];
+            }
+        }
+        return res;
+    }
+};
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int res = 0, sz = nums.size();
+        for(int i = 0; i < sz; ++i){
+            if(i > 0 && i < sz-1 && nums[i] == nums[i-1] && nums[i] == nums[i+1]) {
+                continue;   
+            }
+            nums[res++] = nums[i];
+        }
+        return res;
+    }
+};
+*/
